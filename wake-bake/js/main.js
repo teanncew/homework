f03466648a6719f6d023d7afb0a7c51ac4de4fb5
teanncew <m6.7m@yandex.ru>
2025-10-23 23:09:30 +0300

    gallery

--- wake-bake/js/main.js
+++ wake-bake/js/main.js
@@ -104,6 +104,34 @@ accordionLists.forEach(el => {
 
     });
 
+    // Слайдер-галерея
+
+    const swiper = new Swiper('.gallery__slider', {
+        spaceBetween: 15,
+        slidesPerView: 1.5,
+
+
+        pagination: {
+            el: '.gallery__pagination',
+            type: 'fraction'
+        },
+
+        navigation: {
+            nextEl: '.gallery__next',
+            prevEl: '.gallery__prev',
+        },
+
+        breakpoints: {
+            601: {
+                spaceBetween: 32,
+                slidesPerView: 3,
+            },
+            801: {
+                spaceBetween: 32,
+            },
+            1101: {
+                slidesPerView: 4,
+            },
+        }
+    });
 });
-
-
